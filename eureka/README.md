ccbf3ee305204a44c3caee1ea41d4d6a12dec0bf
Pojo Zhang <pojo.zhang@gmail.com>
2017-07-16 21:41:24 +0800

    update eureka service

---
+++ eureka/README.md
@@ -0,0 +1,6 @@
+### Notice
+**Add following lines to your hosts:**
+```shell
+127.0.0.1 peer1
+127.0.0.1 peer2
+```
--- eureka/src/main/java/sample/eureka/Application.java
+++ eureka/src/main/java/sample/eureka/Application.java
@@ -1,14 +1,27 @@
 package sample.eureka;
 
+import jdk.nashorn.internal.objects.annotations.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaServer
 @SpringBootApplication
+@RestController
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Value("${eureka.instance.hostname}")
+    private String hostname;
+
+    @GetMapping("host")
+    public String host() {
+        return hostname;
     }
 }
