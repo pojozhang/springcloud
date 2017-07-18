50ce29bb00180cb2dc39f6515165fde5cee76937
Pojo Zhang <pojo.zhang@gmail.com>
2017-07-19 00:12:18 +0800

    enable turbine

--- hystrix-service/src/main/java/sample/hystrixservice/Application.java
+++ hystrix-service/src/main/java/sample/hystrixservice/Application.java
@@ -3,9 +3,11 @@ package sample.hystrixservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
-@SpringBootApplication
+@EnableTurbine
 @EnableHystrixDashboard
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
