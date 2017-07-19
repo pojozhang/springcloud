360145b4b071a268dca5b61c321a47d2041a5bcb
pojozhang <pojo.zhang@gmail.com>
2017-07-19 15:39:03 +0800

    add zuul filter

---
+++ zuul-service/src/main/java/sample/zuulservice/filter/FilterType.java
@@ -0,0 +1,9 @@
+package sample.zuulservice.filter;
+
+public interface FilterType {
+
+    String PRE = "pre";
+    String ROUTING = "routing";
+    String POST = "post";
+    String ERROR = "error";
+}
