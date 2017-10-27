399d6f3cd506152f98438be0ad04c6228a655f39
Pojo Zhang <pojo.zhang@gmail.com>
2017-10-28 00:16:59 +0800

    rename module

--- turbine-server/src/main/java/sample/turbineserver/Application.java
+++ turbine-server/src/main/java/sample/turbineserver/Application.java
@@ -1,4 +1,4 @@
-package sample.hystrixserver;
+package sample.turbineserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
